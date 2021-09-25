aa43d75992e900ec6bdb5d7066358cf4b7643958
unknown <ridharra2@gmail.com>
2021-09-25 17:01:47 +0700

    update

--- app/Http/Controllers/Laporan.php
+++ app/Http/Controllers/Laporan.php
@@ -21,29 +21,21 @@ class Laporan extends Controller
     public function uploadLaporan(Request $request){
     $validator = $request->validate([
       'user_id' => 'required',
-      'file' => 'required|mimes:doc,docx,pdf,txt|max:5048',
+      'file' => 'required',
       'area' => 'required',
       'tipe' => 'required',
     ]);
    
 
-    if ($validator->fails()) {
-      return response()->json(['error' => $validator->errors()], 401);
-    }  
-      if ($files = $request->file('file')) {
-
-        $file = $request->file->store('public/pengadilan_negri/'.date('Y').'/'.date('m').'/'.date('d').'/'.'.'.$request->area.'/'.$request->tipe.'/'.$request->user.'/');
-
         $document = new ModelsLaporan();
-        $document->dokumen = $file;
+        $document->dokumen = $request->file;
         $document->user_id = $request->user_id;
         $document->save();
 
         return response()->json([
           "success" => true,
           "message" => "Berhasil Diupload",
-          "file" => $file
+          "file" => $request->file
         ]);
-      }
   }
 }
