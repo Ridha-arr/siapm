ac014859594114fd9430a3ecf4459eb8da7fe298
unknown <ridharra2@gmail.com>
2021-10-02 13:53:43 +0700

    update

---
+++ app/Http/Controllers/verifikasi.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class verifikasi extends Controller
+{
+    public function index(){
+        return view('verifikasi01');
+    }
+}
